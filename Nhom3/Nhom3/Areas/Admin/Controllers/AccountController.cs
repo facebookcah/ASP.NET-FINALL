bf508b78b2ca9a9ba3800986119dcd06b0053d94
PhongND <duyphong05012000@gmail.com>
2021-12-25 02:53:09 +0700

    done

---
+++ Nhom3/Nhom3/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nhom3.Core.Domains;
+
+namespace Nhom3.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        private FlowerContext db = new FlowerContext();
+
+        // GET: Admin/Account
+        public ActionResult Index()
+        {
+            return View(db.TaiKhoans.ToList());
+        }
+
+        // GET: Admin/Account/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // GET: Admin/Account/Create
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+
+        // POST: Admin/Account/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TenTaiKhoan,MatKhau,Quyen,TinhTrang,TenKhachHang,Email,SoDienThoai,DiaChi")] TaiKhoan taiKhoan)
+        {
+            var accounts = db.TaiKhoans.ToList();
+            var exist=accounts.Any(i => i.TenTaiKhoan.ToLower().Equals(taiKhoan.TenTaiKhoan.ToLower()));
+         
+            if (ModelState.IsValid)
+            {
+                if (!exist)
+                {
+                    db.TaiKhoans.Add(taiKhoan);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.duplicate = "no";
+                    ViewBag.Error = "Tên tài khoản đã tồn tại";
+                    return View("Create",taiKhoan);
+                }
+            }
+            else
+            {
+                return View(taiKhoan);
+            }
+        }
+
+        // GET: Admin/Account/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // POST: Admin/Account/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taiKhoan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taiKhoan);
+        }
+
+        // GET: Admin/Account/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            if (taiKhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taiKhoan);
+        }
+
+        // POST: Admin/Account/Delete/5
+       [HttpPost]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(id);
+            var cart = db.GioHangs.ToList();
+            var existInCart = cart.Any(i => i.TenTaiKhoan.Equals(id));
+            if (existInCart)
+            {
+                var cartOfAccount = cart.Where(i => i.TenTaiKhoan.Equals(id)).FirstOrDefault();
+                var cartCode = cartOfAccount.MaGioHang;
+                var countProductInCart = db.ChiTietGioHangs.Where(i=>i.MaGioHang==cartCode).ToList();
+                if (countProductInCart.Count == 0)
+                {
+                    var hoadon = db.HoaDons.ToList().Find(i => i.MaGioHang == cartCode);
+                    db.HoaDons.Remove(hoadon);
+                    db.GioHangs.Remove(cartOfAccount);
+                    db.TaiKhoans.Remove(taiKhoan);
+                   
+                    db.SaveChanges();
+                }
+                else
+                {
+                    ViewBag.Error = "Không xóa được tài khoản! Do tài khoản này đã có đơn hàng";
+                    return View("Delete",taiKhoan);
+                }
+            }
+            
+            db.TaiKhoans.Remove(taiKhoan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Nhom3/Nhom3/Controllers/HomeController.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data.Entity;
+using PagedList;
+using Nhom3.Core.Domains;
+
+namespace Nhom3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        FlowerContext db = new FlowerContext();
+        public ActionResult Index(int? madm, string searchString, int? page)
+        {
+            int pageSize = 8;
+            int pageNumber = (page ?? 1);
+            var product = db.SanPhams.Select(p => p).ToList();
+            /*db.SanPhams.Select(s => s).ToList();*/
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                product = product.Where(s => s.TenSP.ToLower().Contains(searchString.ToLower())).ToList();
+                return View(product.ToPagedList(pageNumber, pageSize));
+            }
+            if (madm > 0)
+            {
+                product = product.Where(p => p.MaDM == madm).ToList();
+                return View(product.Where(s => s.MaDM == madm).ToPagedList(pageNumber, pageSize));
+            }
+            string categoryName = "";
+            if (madm != null)
+            {
+                categoryName += db.DanhMucs.Find(madm).TenDM;
+            }
+            ViewBag.CategoryName = categoryName;
+            return View(product.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Cart()
+        {
+
+
+            return View();
+        }
+
+       
+        public ActionResult Checkout()
+        {
+
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EditUser(string TenTK)
+        {
+
+
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(TenTK);
+            return View(taiKhoan);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditUser([Bind(Include = "TenTaiKhoan,MatKhau,Quyen,TinhTrang," +
+            "TenKhachHang,Email,SoDienThoai,DiaChi")] TaiKhoan taiKhoan, string PassValidate)
+        {
+
+            string Pass = db.TaiKhoans.AsNoTracking().
+                Where(t => t.TenTaiKhoan.Equals(taiKhoan.TenTaiKhoan)).
+                FirstOrDefault().MatKhau;
+            if (Pass.Equals(PassValidate))
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(taiKhoan).State = EntityState.Modified;
+                    db.SaveChanges();
+                    TempData["message1"] = "Thành công";
+                    return RedirectToAction("Index");
+                }
+                ViewBag.Message2 = "Msg2";
+                return View(taiKhoan);
+            }
+            else
+            {
+                ViewBag.Message3 = "Msg3";
+                return View(taiKhoan);
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string TenTaiKhoan, string MatKhau)
+        {
+            if (string.IsNullOrEmpty(TenTaiKhoan))
+            {
+                ViewBag.ErrorTenTaiKhoan = "Tên tài khoản không được để trống";
+            }
+            if (string.IsNullOrEmpty(TenTaiKhoan))
+            {
+                ViewBag.ErrorMatKhau = "Mật khẩu không được để trống";
+            }
+            if (ModelState.IsValid)
+            {
+                var user = db.TaiKhoans.Where(t => t.TenTaiKhoan.Equals(TenTaiKhoan) && t.MatKhau.Equals(MatKhau) && t.Quyen == 0).ToList();
+                if (user.Count() > 0)
+                {
+
+                    if (user.FirstOrDefault().TinhTrang == false)
+                    {
+                        // Hien thi thong bao loi
+                        ViewBag.error = "Tài khoản bị khóa. Đăng nhập không thành công";
+                    }
+                    else
+                    {
+                        //Su dung session: add Session
+                        Session["TaiKhoan"] = user.FirstOrDefault();
+                        Session["TenKhachHang"] = user.FirstOrDefault().TenKhachHang;
+                        Session["TenTaiKhoan"] = user.FirstOrDefault().TenTaiKhoan;
+                        // Sang trang chu
+                        return RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    ViewBag.error = "Đăng nhập không thành công";
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Users(string TenTK)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(TenTK);
+            return View(taiKhoan);
+        }
+        public ActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sp = db.SanPhams.Find(id);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+        public PartialViewResult _Nav()
+        {
+            var danhmuc = db.DanhMucs.Select(p => p);
+            return PartialView(danhmuc);
+        }
+        [HttpGet]
+        public ActionResult Signin()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Signin([Bind(Include = "TenTaiKhoan,MatKhau,Quyen,TinhTrang,TenKhachHang,Email,SoDienThoai,DiaChi")] TaiKhoan taiKhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                var taiKhoanFind = db.TaiKhoans.Find(taiKhoan.TenTaiKhoan);
+                if (taiKhoanFind == null)
+                {
+                    db.TaiKhoans.Add(taiKhoan);
+                    db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    ViewBag.ErrorSign = "Tên tài khoản trùng. Vui lòng nhập tên khác";
+                }
+            }
+            //ViewBag.Infor = taiKhoan.ToString();
+            return View(taiKhoan);
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult ChangePassword(string TenTK)
+        {
+            TaiKhoan taiKhoan = db.TaiKhoans.Find(TenTK);
+            return View(taiKhoan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangePassword([Bind(Include = "TenTaiKhoan,MatKhau,Quyen,TinhTrang,TenKhachHang,Email,SoDienThoai,DiaChi")] TaiKhoan taiKhoan,
+            string OldPassword)
+        {
+            string old_pass = db.TaiKhoans.AsNoTracking().
+                Where(t => t.TenTaiKhoan.Equals(taiKhoan.TenTaiKhoan)).
+                FirstOrDefault().MatKhau;
+            if (old_pass.Equals(OldPassword))
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(taiKhoan).State = EntityState.Modified;
+                    db.SaveChanges();
+                    TempData["message"] = "Thành công";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Message = "Lỗi nhập dữ liệu!!!";
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Mật khẩu cũ không chính xác!!!";
+            }
+            return View(taiKhoan);
+        }
+
+    }
+
+
+}
+
